2e4af314f1c6040951f48d3c955c20443604f13c
Hugo Duncan <hugo@hugoduncan.org>
2012-10-01 16:48:19 -0400

    Add top level project.clj using lein-sub

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject hornetq-clj "0.2.0-SNAPSHOT"
+  :description "hornetq clj interface"
+  :url "https://github.com/hugoduncan/hornetq-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-sub "0.2.3"]]
+  :sub ["client" "server" "lein-hornetq" "stomp-example"]
+  :aliases {"clean" ["sub" "clean"]
+            "install" ["sub" "install"]
+            "deploy" ["sub" "deploy"]
+            "test" ["sub" "test"]
+            "doc" ["sub" "with-profile" "codox,jdk1.7" "doc"]
+            "set-sub-version" ["sub" "with-profile" "release" "set-version"]})
