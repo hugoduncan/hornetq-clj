b344ae9134e69dc637233844d01b72f3d88492ab
Dan Lentz <danlentz@gmail.com>
2014-07-04 13:56:19 -0400

    updated hornetq dependencies

--- client/project.clj
+++ client/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.hornetq/hornetq-core-client "2.2.18.Final"]
+                 [org.hornetq/hornetq-core-client "2.3.17.Final"]
                  [org.jboss.netty/netty "3.2.1.Final"]]
   :repositories {"JBoss releases"
                  "http://repository.jboss.org/nexus/content/groups/public-jboss/"})
--- server/project.clj
+++ server/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.hornetq/hornetq-core "2.2.18.Final"]
-                 [org.hornetq/hornetq-logging "2.2.18.Final"]
+                 [org.hornetq/hornetq-core "2.2.21.Final"]
+                 [org.hornetq/hornetq-logging "2.2.21.Final"]
                  [org.jboss.netty/netty "3.2.1.Final"]]
   :repositories {"JBoss releases"
                  "http://repository.jboss.org/nexus/content/groups/public-jboss/"})
