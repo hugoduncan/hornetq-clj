0fbc3cee215c5b6000840d86185b8ec4f94833e5
Hugo Duncan <hugo@hugoduncan.org>
2014-07-18 14:21:53 -0400

    Fix lein plugin options processing
    Now uses --disable-security to disable server security.
    
    Uses --user and --password to set the cluster user and
    password (defaults to admin/password).

--- lein-hornetq/src/leiningen/hornetq_server.clj
+++ lein-hornetq/src/leiningen/hornetq_server.clj
@@ -1,7 +1,9 @@
 (ns leiningen.hornetq-server
   "A leiningen plugin to start a HornetQ server."
-  (:use
-   [clojure.tools.cli :only [cli]]))
+  (:require
+   [clojure.string :as string]
+   [clojure.tools.cli :refer [parse-opts]]
+   [leiningen.core.main :refer [debug]]))
 
 (defn eval-in-project
   "Support eval-in-project in both Leiningen 1.x and 2.x."
@@ -14,17 +16,37 @@
                      (catch java.io.FileNotFoundException _)))]
     (apply eip args)))
 
-(def deps '[[hornetq-clj/server "0.2.1"]
+(def deps '[[hornetq-clj/server "0.2.2-SNAPSHOT"]
+            [org.clojure/clojure "1.6.0"]
             [org.slf4j/jul-to-slf4j "1.6.4"]
             [ch.qos.logback/logback-classic "1.0.0"]])
+
+(def cli-options
+  [["-s" "--stomp PORT"
+    "Use stomp (port if specified)" :default false]
+   ["-n" "--netty PORT"
+    "Use netty (port if specified)" :default true]
+   ["-u" "--user USER"
+    "Set the cluster user" :default "admin"]
+   ["-p" "--password PASSWORD"
+    "Set the cluster password" :default "password"]
+   ["-S" "--disable-security"
+    "Disable security" :default false]
+   ["-P" "--persistence-enabled"
+    "Enable persistence" :default true]])
 
 (defn hornetq-server
   "Launch hornetq server"
   [project & args]
-  (let [[{:keys [netty stomp] :as opts} args]
-        (cli args
-             ["-s" "--stomp" "Use stomp (port if specified)" :default false]
-             ["-n" "--netty" "Use netty (port if specified)" :default true])]
+  (let [{:keys [errors options]} (parse-opts args cli-options)
+        {:keys [netty stomp] :as opts} (-> options
+                                           (assoc :enable-security
+                                             (not (:disable-security options)))
+                                           (dissoc :disable-security))]
+    (when errors
+      (throw (ex-info (string/join errors) {:exit-code 1})))
+    (debug "options" options)
+    (debug "opts" opts)
     (eval-in-project
      (assoc-in project [:dependencies] deps)
      `(do (require '~'hornetq-clj.server)
